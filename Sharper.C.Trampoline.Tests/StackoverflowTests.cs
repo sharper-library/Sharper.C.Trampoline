e6e610557ff98a06d72106ed1093f01817eca56a
Jeremy Hughes <jeremy@tflite.com>
2015-11-17 12:09:48 +1300

    Comment out broken code

--- Sharper.C.Trampoline.Tests/StackoverflowTests.cs
+++ Sharper.C.Trampoline.Tests/StackoverflowTests.cs
@@ -73,23 +73,23 @@ public static class ChainTests
           , s => s.Eval()
           );
 
-    public static Gen<Chain<A>> Sequence<A>(Chain<Gen<A>> xs)
-    =>
-        xs.Eval().FoldRight
-          ( Gen.Constant(EndChain<A>())
-          , (ga, x) =>
-                x.Map
-                  ( gsa =>
-                        from sa in gsa
-                        from a in ga
-                        select YieldLink(a, sa)
-                  )
-          )
-        .Eval();
+    // public static Gen<Chain<A>> Sequence<A>(Chain<Gen<A>> xs)
+    // =>
+    //     xs.Eval().FoldRight
+    //       ( Gen.Constant(EndChain<A>())
+    //       , (ga, x) =>
+    //             x.Map
+    //               ( gsa =>
+    //                     from sa in gsa
+    //                     from a in ga
+    //                     select YieldLink(a, sa)
+    //               )
+    //       )
+    //     .Eval();
 
-    public static Gen<IEnumerable<A>> Sequence<A>(IEnumerable<Gen<A>> xs)
-    =>
-        Sequence(xs.ToChain()).Select(sa => sa.Eval());
+    // public static Gen<IEnumerable<A>> Sequence<A>(IEnumerable<Gen<A>> xs)
+    // =>
+    //     Sequence(xs.ToChain()).Select(sa => sa.Eval());
 }
 
 public static class StepTests
